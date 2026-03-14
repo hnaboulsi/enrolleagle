85e53c0d2875a20e146b5740df6e200e45f38ec9
hnaboulsi <hassannaboulsi@gmail.com>
2026-03-13 18:34:00 -0700

    Revert "Add Vero menu bar app and backend fixes"
    This reverts commit 006db476b12f8bcbf02ec00e040a4248a31ebf84.

--- vero/mac_client/tracker_test.py
+++
@@ -1,6 +0,0 @@
-def test_tracker_module_imports():
-    import tracker  # noqa: F401
-
-
-def test_notifier_module_imports():
-    import notifier  # noqa: F401
--- vero/mac_native/Shared/AppConstants.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-enum AppConstants {
-    static let appGroupIdentifier = "group.com.naboulsi.vero"
-    static let mainBundleIdentifier = "com.naboulsi.vero"
-    static let agentBundleIdentifier = "com.naboulsi.vero.agent"
-    static let urlScheme = "vero"
-    static let calendarName = "Vero"
-}
